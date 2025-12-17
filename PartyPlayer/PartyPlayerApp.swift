813cb0a9b2bedba02e45d27839efdef1c57f8ffa
peterknapp <4532026+peterknapp@users.noreply.github.com>
2025-12-17 14:06:47 +0100

    Initial Commit

---
+++ PartyPlayer/PartyPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyPlayerApp.swift
+//  PartyPlayer
+//
+//  Created by Peter Knapp on 17.12.25.
+//
+
+import SwiftUI
+
+@main
+struct PartyPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
