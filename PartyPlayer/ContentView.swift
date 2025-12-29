f2674cc71113662da19fb732fe21e923631f4b6f
Peter Knapp <pknapp@newyorker.de>
2025-12-29 13:18:38 +0100

    Push Test

--- PartyPlayer/ContentView.swift
+++ PartyPlayer/ContentView.swift
@@ -67,7 +67,7 @@ struct ContentView: View {
                     VStack(spacing: 16) {
                         Text("Party Player").font(.largeTitle.bold())
                         HStack {
-                            Button("Party anlegen") {
+                            Button("Party neu anlegen") {
                                 pendingAdminCodeSetup = true
                             }
                             Button("Party beitreten") {
